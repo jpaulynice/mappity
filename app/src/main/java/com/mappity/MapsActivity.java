dee2fd5108afa25867c5ec3a964284f901664d1b
julesbond007 <jay.paulynice@gmail.com>
2016-03-26 23:09:35 -0400

    first commit

---
+++ app/src/main/java/com/mappity/MapsActivity.java
@@ -0,0 +1,34 @@
+package com.mappity;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng boston = new LatLng(42.364506, -71.038887);
+        map.addMarker(new MarkerOptions().position(boston).title("Marker in " +
+                "Boston, MA"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(boston));
+    }
+}
